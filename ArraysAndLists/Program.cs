9727609d77b2ded3c6dfb8d9c634adddb0292730
tgendrich <gendricht@gmail.com>
2021-07-25 20:19:05 -0500

    work

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ArraysAndLists
 {
@@ -9,6 +11,21 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> evenNumbers = new List<int>();
+            List<int> oddNumbers = new List<int>();
+
+            foreach (int element in numbers)
+                if (element % 2 == 0)
+                    evenNumbers.Add(element);
+                else
+                    oddNumbers.Add(element);
+            var evensAndOdds = evenNumbers.Zip(oddNumbers, (first, second) => "evens "+ first + " " + " odds " + second);
+           foreach (var element in evensAndOdds)
+                Console.WriteLine(element);
+
+
+
 
             /* Create two Lists of type int.
              * Name one List "evens"
